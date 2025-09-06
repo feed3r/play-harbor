2ad5b5d592de1a8fe06439011fa26cdafddc7822
Alessio Pascucci <feeder81@gmail.com>
2025-09-06 17:16:34 +0200

    [REFACTOR] Revisit game management
    When launching the game, check if the game manager is already running (in this case it's the EpicGamesLauncher)  to evaluate a different time to sleep.
    
    Signed-off-by: Alessio Pascucci <feeder81@gmail.com>

--- go-launcher/runlauncher/runlauncher.go
+++ go-launcher/runlauncher/runlauncher.go
@@ -2,6 +2,7 @@ package runlauncher
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/feed3r/play-harbor/go-launcher/args"
@@ -9,21 +10,90 @@ import (
 	"github.com/feed3r/play-harbor/go-launcher/processutil"
 )
 
+const SLEEP_WITHOUT_MANAGER = 30 * time.Second
+const SLEEP_WITH_MANAGER = 10 * time.Second
+
+const MANAGER_EXECUTABLE_NAME = "EpicGamesLauncher.exe"
+const MAX_POLLING_ATTEMPTS = 20
+const POLLING_INTERVAL = 1 * time.Second
+
+// Global function variables to allow mocking in tests
+var LaunchGameFunc = launcher.LaunchGame
+var PollGameProcessFunc = PollGameProcess
+var WaitForProcessExitFunc = processutil.WaitForProcessExit
+
+// Check if the game manager platform client (IE: the Epic Games Launcher)
+// is active
+var IsManagerRunning = func(executableName string) (bool, error) {
+	procs, err := processutil.FindExecutableProcess(executableName)
+	if err != nil {
+		if strings.Contains(err.Error(), "could not find a process with name") {
+			return false, nil // Not running, but not a real error
+		}
+		return false, err
+	}
+	return procs != nil, nil
+}
+
+var SleepFunc = func() {
+	time.Sleep(SLEEP_WITHOUT_MANAGER)
+}
+
 // Orchestrator for launching the game
 func RunLauncher(argsList []string) error {
 	epicUrl, exeName, err := args.ParseArgs(argsList)
 	if err != nil {
 		return err
 	}
-	if err := launcher.LaunchGame(epicUrl); err != nil {
+
+	// Check if the game manager is running
+	managerRunning, err := IsManagerRunning(MANAGER_EXECUTABLE_NAME)
+	if err != nil {
+		return fmt.Errorf("error checking for game manager process: %v", err)
+	}
+
+	if managerRunning {
+		fmt.Println("Game manager detected, using shorter wait time")
+		SleepFunc = func() {
+			time.Sleep(SLEEP_WITH_MANAGER)
+		}
+	} else {
+		fmt.Println("No game manager detected, using longer wait time")
+		// SleepFunc remains as default
+	}
+
+	if err := LaunchGameFunc(epicUrl); err != nil {
 		return fmt.Errorf("failed to start URL: %v", err)
 	}
+
 	// Wait a few seconds to give the game time to start
 	// You can modularize this in the future
-	time.Sleep(30 * time.Second)
-	proc, err := processutil.FindGameProcess(exeName)
+	SleepFunc()
+
+	// Find the game process with a polling
+	proc, err := PollGameProcessFunc(exeName)
 	if err != nil {
 		return err
 	}
-	return processutil.WaitForProcessExit(proc)
+
+	return WaitForProcessExitFunc(proc)
+}
+
+// Find the game process executing. Poll actively for the process
+// for a maximum amount of time
+func PollGameProcess(exeName string) (processutil.ProcessLike, error) {
+	pollingTrialCounter := MAX_POLLING_ATTEMPTS
+
+	for pollingTrialCounter > 0 {
+		pollingTrialCounter--
+		proc, err := processutil.FindExecutableProcess(exeName)
+		if err != nil {
+			return nil, err
+		} else if proc != nil {
+			fmt.Printf("Found game process with PID %d\n", proc.Pid())
+			return proc, nil
+		}
+		time.Sleep(POLLING_INTERVAL)
+	}
+	return nil, fmt.Errorf("game process not found after %d attempts", MAX_POLLING_ATTEMPTS)
 }
